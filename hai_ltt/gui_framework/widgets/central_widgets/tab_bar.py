d8b448f0ea9c415643222eaa1d2babfcd19c1ed8
zhangzhengde0225 <zhangzhengde0225@gmail.com>
2023-01-03 18:46:23 +0800

    zzd

--- hai_ltt/gui_framework/widgets/central_widgets/tab_bar.py
+++ hai_ltt/gui_framework/widgets/central_widgets/tab_bar.py
@@ -14,7 +14,6 @@ class HTabBar(QTabBar):  # HAI TabBar
         self.c_tabbar = QTabBar(self.parent)
         # 设置self左对齐
 
-
         self.tabCloseRequested.connect(self.on_tabCloseRequested)
 
     def mousePressEvent(self, ev):
@@ -41,14 +40,23 @@ class HTabBar(QTabBar):  # HAI TabBar
     def mouseMoveEvent(self, ev):
         print(f'mouseMoveEvent: {ev}')
         # 获取当前鼠标对应的tab
-        tab_index = self.tabAt(ev.pos())
-        if tab_index == -1:
+        tab_idx = self.tabAt(ev.pos())
+        if tab_idx == -1:
             return
+        
+        # 当前tabbar跟随鼠标移动
         self.c_tabbar.show()
         w, h = self.c_tabbar.sizeHint().width(), self.c_tabbar.sizeHint().height()
         x, y = int(ev.x()-w/2), int(ev.y()-h/2)
         self.c_tabbar.move(x, y)
+        # 如果移动到其他标签处，切换标签
+        tab_idx2 = self.tabAt(ev.pos())  # 第二个标签
+        if tab_idx2 != -1 and tab_idx2 != tab_idx:
+            self.moveFromTo(tab_idx, tab_idx2)
 
+
+    def mouseReleaseEvent(self, ev):
+        self.c_tabbar.hide()
 
     def on_tabCloseRequested(self, index):
         logger.info(f'on_tabCloseRequested: {index}')
